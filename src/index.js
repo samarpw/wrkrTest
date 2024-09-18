781304616b2c1e4b0ab680b230080882352a0a91
Samar <36756790+samarpw@users.noreply.github.com>
2024-09-18 19:06:12 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,43 @@
 // Export a default object containing event handlers
 export default {
-  // The fetch handler is invoked when this worker receives a HTTP(S) request
-  // and should return a Response (optionally wrapped in a Promise)
-  async fetch(request, env, ctx) {
-    // You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
-    const url = new URL(request.url);
-
-
-    // You can get pretty far with simple logic like if/switch-statements
-    switch (url.pathname) {
-      case '/sub':
-        return new Response('subing');
-    }
-
-
-    if (url.pathname.startsWith('/api/')) {
-      // You can also use more robust routing
-      return new Response('aping');
-    }
-
-
+	// The fetch handler is invoked when this worker receives a HTTP(S) request
+	// and should return a Response (optionally wrapped in a Promise)
+	async fetch(request, env, ctx) {
+		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
+		const url = new URL(request.url);
+		
+		
+		
+		// You can get pretty far with simple logic like if/switch-statements
+		switch (url.pathname) {
+			case '/sub':
+			return new Response('subing');
+		}
+		
+		
+		if (url.pathname.startsWith('/api/')) {
+			// You can also use more robust routing
+			return new Response(
+				'aping',
+				{headers:{"Content-Type":"text/plain", 
+						  "Access-Control-Allow-Origin":"*", 
+						  "Access-Control-Allow-Methods":"OPTIONS, POST, GET, DELETE, PUT", 
+						  "Access-Control-Allow-Headers":"x-requested-with, Content-Type, origin, authorization, accept, credentials, client-security-token",
+						  "Access-Control-Max-Age":"600"
+					}
+				}
+			);
+		}
+		
+		
 		
 		return new Response(
 			`I Try making requests to:
-      <ul>
-      <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
-      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
-      <li><code><a href="/api/todos">/api/todos</a></code></li>`,
-			{ headers: { "Content-Type": "text/plain" } }
+			<ul>
+			<li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
+			<li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
+			<li><code><a href="/api/todos">/api/todos</a></code></li>`,
+			{headers:{"Content-Type":"text/plain"}}
 		);
-  },
+	},
 };
